df67d8475f25646fce16bd8cf66be44f25f6c2b0
roh-ann23 <158121284+roh-ann23@users.noreply.github.com>
2024-04-17 14:20:48 +0530

    committed

--- config/db.js
+++ config/db.js
@@ -3,30 +3,44 @@ import  mongoose  from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const MONGODB_URI = `${process.env.MONGO_URI}`
-// Set Up mongoDB connection
-mongoose.connect(MONGODB_URI  , {
-    useNewUrlParser : true,
-    useUnifiedTopology:true
+
+// const mongoose = require('mongoose');
+
+const db = mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((error) => {
+  console.error("Error connecting to MongoDB:", error);
 });
 
 
-// Set up default connection
-//mongoose maintains default connection object representing mongoDB connection 
-const db = mongoose.connection;
 
-// Define event listener for databases 
+// const MONGODB_URI = `${process.env.MONGO_URI}`
+// // Set Up mongoDB connection
+// mongoose.connect(MONGODB_URI  , {
+//     useNewUrlParser : true,
+//     useUnifiedTopology:true
+// });
 
-db.on('connected',()=>{
-    // console.log("Connected to Database");
-})
 
-db.on('error',(err)=>{
-    // console.log("Error on Database" + err);
-})
+// // Set up default connection
+// //mongoose maintains default connection object representing mongoDB connection 
+// const db = mongoose.connection;
 
-db.on('disconnected',()=>{
-    // console.log("Disonnected to Database");
-})
+// // Define event listener for databases 
+
+// db.on('connected',()=>{
+//     // console.log("Connected to Database");
+// })
+
+// db.on('error',(err)=>{
+//     // console.log("Error on Database" + err);
+// })
+
+// db.on('disconnected',()=>{
+//     // console.log("Disonnected to Database");
+// })
 
 export default db;
